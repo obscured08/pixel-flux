d751f0c9dcc3cfac4032c7abfa36588593e82681
obscured <obscured08@gmail.com>
2026-02-26 15:49:07 -0600

    changing from float to int value for char len

--- worker.py
+++ worker.py
@@ -69,7 +69,8 @@ async def process_image(input_path, mask_path, params_json, progress_callback=No
             cur_mask = mask_img.resize(work_frame.size)
 
         try:
-            print(f"Frame {i}: Mode={params['interval_func']}, CL={cur_cl:.2f}, TL={cur_tl:.2f}, TU={cur_tu:.2f}")
+            print(f"Frame {i}: Mode={params['interval_func']}, CL={cur_cl}, TL={cur_tl:.2f}, TU={cur_tu:.2f}")
+            
             
             sorted_frame = ps_func(
                 work_frame,
